dcaf5d3728c0728fc62b56d2f2f3b22291e10b29
lifenli <67099469+lifenli@users.noreply.github.com>
2024-03-16 16:44:58 +0800

    add categories

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -6,17 +6,36 @@ interface FormDataProps {
     date: Date,
     title: string,
     slug: string,            
-    category: string,
+    categoryNames: string[]; 
     body: string,
 }
 
 export async function createPost(formData: FormDataProps) {
-    const { date, slug, category, title, body } = formData;
+    const { date, slug, categoryNames, title, body } = formData;
+
+    // Find existing categories by name
+    const categories = await db.category.findMany({
+        where: { name: { in: categoryNames } },
+    });
+
+    // Determine missing category names
+    const missingCategoryNames = categoryNames.filter(name => !categories.some(category => category.name === name));
+
+    // Create missing categories
+    const createdCategories = await Promise.all(
+        missingCategoryNames.map(name => db.category.create({ data: { name } }))
+    );
+
+    // Combine existing and newly created categories
+    const allCategories = [...categories, ...createdCategories];
+
+    // Extract category IDs
+    const categoryIDs = allCategories.map(category => category.id);
     const post = await db.post.create({
         data: {
             date,
             slug,
-            category,
+            categoryIDs,
             title,
             body,
         }
--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,80 +1,3 @@
-//  import { redirect } from "next/navigation";
-//  import { db } from "@/db";
-//  import { parse, formatISO } from 'date-fns'; 
-
-// export default function CreatePost() {
-//     async function createPost(formData: FormData) {
-//          'use server';
-//          const dateString = formData.get('date') as string;
-//          const date = parse(dateString, 'yyyyMMdd', new Date()); 
-//          const isoDateString = formatISO(date); 
-//          const slug = formData.get('slug') as string;
-//          const title = formData.get('title') as string;
-//          const body = formData.get('body') as string;
-//          const post = await db.post.create({
-//             data: {
-//                 date: isoDateString,
-//                 slug,
-//                 title,
-//                 body
-//             }
-//          });
-//          console.log(post);
-//          console.log(date);
-         
-//          redirect('/dashboard');
-//     }
-
-//     return(
-//         <form action={createPost}>
-//             <h3 className="text-center mb-8">Create a new blog</h3>
-//         <div className="flex flex-col gap-4 p-5">
-//         <div className="flex gap-4">
-//                 <label htmlFor="date" className="w-20">Date</label>
-//                 <input 
-//                 className="border rounded p-2"
-//                 name="date"
-//                 type="text" 
-//                 id="date"
-//                 />
-//             </div>
-//             <div className="flex gap-4">
-//                 <label htmlFor="slug" className="w-20">Slug</label>
-//                 <input 
-//                 className="border rounded p-2 w-full"
-//                 name="slug"
-//                 type="text" 
-//                 id="slug"
-//                 />
-//             </div>
-//              <div className="flex gap-4">
-//                 <label htmlFor="title" className="w-20">Title</label>
-//                 <input 
-//                 className="border rounded p-2 w-full"
-//                 name="title"
-//                 type="text" 
-//                 id="title"
-//                 />
-//             </div>
-//             <div className="container flex gap-4">
-//                 <label htmlFor="body" className="w-20">Content Body</label>
-//                 <textarea 
-//                 className="border rounded p-2 w-full"
-//                 name="body" 
-//                 rows={10}
-//                 id="body"
-//                 />
-//             </div>
-//             <div className="flex gap-4 justify-end">
-//             <button className="rounded bg-blue-500 shadow-md text-zinc-200 hover:text-zinc-900 shadow-stone-600 px-4 py-2">
-//              Submit
-//             </button>
-//             </div>
-//         </div>
-//         </form>
-//     )
-// }
-
 'use client'
 import * as action from '@/actions'
 import React, { useState } from "react";
@@ -93,7 +16,7 @@ interface FormDataProps {
     date: Date;
     slug: string;
     title: string;
-    category: string;
+    categoryNames: string[];
     body: string;
 }
 
@@ -113,7 +36,9 @@ function createSlug(title: string) {
 export default function CreatePost() {
     const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
     const [title, setTitle] = useState('');
-    const [category, setCategory] = useState('');
+    const categories = ['Thoughts', 'Hobby', 'Work'];
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
 
     const editor = useEditor({
         extensions: [
@@ -135,14 +60,14 @@ export default function CreatePost() {
       })
     const editorContent = editor?.getHTML();
 
- const widthRef = React.useRef<HTMLInputElement>(null);
-  const heightRef = React.useRef<HTMLInputElement>(null);
+    const widthRef = React.useRef<HTMLInputElement>(null);
+    const heightRef = React.useRef<HTMLInputElement>(null);
   
-  React.useEffect(() => {
-    if (widthRef.current && heightRef.current) {
-      setWidthAndHeight();
-    }
-  }, [])
+    React.useEffect(() => {
+        if (widthRef.current && heightRef.current) {
+        setWidthAndHeight();
+        }
+    }, [])
 
   const setWidthAndHeight = () => {
     widthRef.current!.value = '320';
@@ -156,8 +81,8 @@ export default function CreatePost() {
           const widthValue = widthRef.current?.value ? parseInt(widthRef.current.value, 10) : null;
           const heightValue = heightRef.current?.value ? parseInt(heightRef.current.value, 10) : null;
       
-          const width = Math.max(320, widthValue || 640);
-          const height = Math.max(180, heightValue || 480);
+          const width = Math.max(320, widthValue || 448);
+          const height = Math.max(180, heightValue || 336);
       
           editor?.commands.setYoutubeVideo({
             src: url,
@@ -168,13 +93,18 @@ export default function CreatePost() {
       };
       
 
+const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedOptions = Array.from(e.target.selectedOptions, (option) => option.value);
+        setSelectedCategories(selectedOptions);
+    };
+
 const handleSumbit = async (event: React.FormEvent) => {
     event.preventDefault();
     const generatedSlug = createSlug(title); 
     const formData: FormDataProps = {
     date: selectedDate || new Date(),
     title,
-    category,
+    categoryNames: selectedCategories,
     slug: generatedSlug,
     body: editorContent ?? '',
 };
@@ -200,14 +130,17 @@ const handleSumbit = async (event: React.FormEvent) => {
                     <label htmlFor="category" className="w-20">Category</label>
                     <select
                         className="border rounded p-2"
-                        onChange={(e) => setCategory(e.target.value)}
+                        onChange={handleCategoryChange}
                         name="category"
                         id="category"
+                        multiple // Allow multiple selections
+                        value={selectedCategories} // Controlled component
                     >
-                        <option value="">Select a category</option>
-                        <option value="Thoughts">Thoughts</option>
-                        <option value="Hobby">Hobby</option>
-                        <option value="Work">Work</option>
+                        {categories.map((category) => (
+                            <option key={category} value={category}>
+                                {category}
+                            </option>
+                        ))}
                     </select>
                     </div>
                      <div className="flex gap-4">
