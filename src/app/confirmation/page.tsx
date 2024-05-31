c624c6f61f07f73b9fe493636c6cc5313be7a431
lifenli <67099469+lifenli@users.noreply.github.com>
2024-05-31 15:47:50 +0800

    refine contact page with zod validation

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -1,26 +1,23 @@
 'use client';
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 const OrderConfirmation = () => {
-    const searchPath = useSearchParams() 
-    const sessionId = searchPath.get('session_id')
+  const searchPath = useSearchParams();
+  const sessionId = searchPath.get('session_id');
 
-if (sessionId) {
+  if (sessionId) {
     return (
-        <Suspense>
-        <div>Thanks for your order. We will send you an email to confirm the date</div>
-        <div>You can visit this page to manage your subscriptions.</div>
-        </Suspense>
-    )
-} else {
-    return (
+      <Suspense>
         <div>
-            No order found!
+          Thanks for your order. We will send you an email to confirm the date
         </div>
-    )
-}
- 
+        <div>You can visit this page to manage your subscriptions.</div>
+      </Suspense>
+    );
+  } else {
+    return <div>No order found!</div>;
+  }
 };
 
 export default OrderConfirmation;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,67 +1,104 @@
-'use client';
-import React, { useState, FormEvent } from 'react';
-import {Button, Input, Textarea} from "@nextui-org/react";
- 
+'use client'
+import React, { useState, FormEvent } from 'react'
+import { Button, Input, Textarea } from '@nextui-org/react'
+
 export default function ContactPage() {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [error, setError] = useState<string | null>(null)
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
- 
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    setIsLoading(true)
-    setError(null) // Clear previous errors when a new request starts
- 
-    try {
-      const formData = new FormData(event.currentTarget);
-      const email = formData.get('email') as string;
-      const name = formData.get('name') as string;
-      const message = formData.get('message') as string;
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [errors, setErrors] = useState<{ [key: string]: string | null }>({})
+    const [successMessage, setSuccessMessage] = useState<string | null>(null)
 
-      console.log({ email, name, message }); // Debugging log
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        setIsLoading(true)
 
-      const response = await fetch('/api/email', {
-        method: 'POST',
-        body: JSON.stringify({
-          email,
-          name,
-          message,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
- 
-      if (!response.ok) {
-        throw new Error('Failed to submit the data. Please try again.')
-      }
-      // const data = await response.json() 
-      // Display success message
-      setSuccessMessage('Your message has been sent successfully!');
-       // Optionally, reset the form
-    } catch (error: any) {
-      // Capture the error message to display to the user
-      setError(error.message)
-      console.error(error)
-    } finally {
-      setIsLoading(false)
+        try {
+            const formData = new FormData(event.currentTarget)
+            const email = formData.get('email') as string
+            const name = formData.get('name') as string
+            const description = formData.get('description') as string
+
+            console.log({ email, name, description }) // Debugging log
+
+            const response = await fetch('/api/email', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email,
+                    name,
+                    description,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            if (!response.ok) {
+                const data = await response.json()
+                if (data.errors) {
+                    const newErrors: { [key: string]: string } = {}
+                    for (const key in data.errors) {
+                        if (data.errors[key]) {
+                            newErrors[key] = data.errors[key].join(', ')
+                        }
+                    }
+                    setErrors(newErrors)
+                    return
+                }
+                throw new Error('Failed to submit the data. Please try again.')
+            }
+            // Display success message
+            setSuccessMessage(
+                'Thank you for reaching out. We will get back to you soon.'
+            )
+        } catch (error: any) {
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+        }
     }
-  }
- 
-  return (
-    <div>
-        <h2>Have Questions? Drop us a line!</h2>
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-      {successMessage ? <div style={{ color: 'green' }}>{successMessage}</div>
-      : (<form onSubmit={onSubmit}>
-        <Input type="email" name="email" label="Email" required />
-        <Input type="text" name="name" label="Name" required />
-        <Textarea name="message" label="Message" required />
-        <Button type="submit" disabled={isLoading}>
-          {isLoading ? 'Loading...' : 'Submit'}
-        </Button>
-      </form>) 
-      }
-    </div>
-  )
+
+    return (
+        <div className="text-center">
+            <h2>Have Questions? Drop us a line!</h2>
+            {successMessage ? (
+                <div style={{ color: 'green' }}>{successMessage}</div>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-6">
+                    <form
+                        onSubmit={onSubmit}
+                        className="flex flex-col gap-4 col-span-1 sm:col-start-2 sm:col-span-4 w-full"
+                    >
+                        <Input
+                            type="email"
+                            name="email"
+                            label="Email"
+                            isInvalid={!!errors.email}
+                            errorMessage={errors.email}
+                            required
+                        />
+                        <Input
+                            type="text"
+                            name="name"
+                            label="Name"
+                            isInvalid={!!errors.name}
+                            errorMessage={errors.name}
+                            required
+                        />
+                        <Textarea
+                            name="description"
+                            label="Description"
+                            isInvalid={!!errors.description}
+                            errorMessage={errors.description}
+                            required
+                        />
+                        <Button
+                            type="submit"
+                            disabled={isLoading}
+                            color="primary"
+                        >
+                            {isLoading ? 'Loading...' : 'Submit'}
+                        </Button>
+                    </form>
+                </div>
+            )}
+        </div>
+    )
 }
