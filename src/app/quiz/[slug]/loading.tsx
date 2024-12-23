4283ac36a8b30e7e4535b6ab112b2a3beded533a
lifenli <67099469+lifenli@users.noreply.github.com>
2024-12-23 16:20:08 +0700

    remove a redundant loading page for quiz slug page

--- src/app/quiz/[slug]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { FullSkeleton } from '@/components/common/skeleton-loading'
-
-export default function QuizLoading() {
-    return (
-        <div className="gap-3">
-            <FullSkeleton />
-        </div>
-    )
-}
